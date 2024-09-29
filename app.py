a5ede089fc1a778fbc3bb2ee6fc11c16e9a7ca00
mariacristinafadgyas <fadgyas.maria@yahoo.com>
2024-09-29 19:10:08 +0200

    added route to fetch the characters with pagination

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import random
 from storage import read_data
 
 app = Flask(__name__)
@@ -9,7 +10,37 @@ def get_characters():
     """Returns a JSON file containing the list of characters from a Game of Thrones."""
 
     characters = read_data('characters.json')
-    return jsonify(characters), 200
+
+    limit = 20
+    skip = 0
+
+    if 'limit' in request.args:
+        try:
+            limit = int(request.args.get('limit'))
+            if limit <= 0:
+                return jsonify({"error": "Limit must be greater than 0."}), 400
+        except ValueError:
+            return jsonify({"error": "Invalid limit parameter. Must be an integer."}), 400
+
+    if 'skip' in request.args:
+        try:
+            skip = int(request.args.get('skip'))
+            if skip < 0:
+                return jsonify({"error": "Skip must be a non-negative integer."}), 400
+        except ValueError:
+            return jsonify({"error": "Invalid skip parameter. Must be an integer."}), 400
+
+    if skip >= len(characters):
+        return jsonify({"message": f"Skip is exceeding the length of the"
+                                   f" characters database (Total characters: {len(characters)})"}), 400
+
+    if 'limit' not in request.args and 'skip' not in request.args:
+        random_characters = random.sample(characters, min(20, len(characters)))
+        return jsonify(random_characters), 200
+
+    paginated_characters = characters[skip:skip + limit]
+
+    return jsonify(paginated_characters), 200
 
 
 if __name__ == '__main__':
