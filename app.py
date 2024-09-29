cc2a066826f4d09814f10aed167b058ce61e5164
mariacristinafadgyas <fadgyas.maria@yahoo.com>
2024-09-29 21:44:00 +0200

    added sort feature

--- app.py
+++ app.py
@@ -13,6 +13,25 @@ def get_characters():
 
     characters = read_data('characters.json')
 
+    # Sort by any of the character's attributes
+    sort_asc = request.args.get('sort_asc')
+    sort_desc = request.args.get('sort_desc')
+    attributes = ['age', 'animal', 'death', 'house', 'id', 'name', 'nickname', 'role', 'strength', 'symbol']
+
+    if (sort_asc and sort_asc not in attributes) or (sort_desc and sort_desc not in attributes):
+        return jsonify({"error": "To sort please select one of these attributes: 'age' / 'animal' /"
+                                 " 'death' / 'house' / 'id' / 'name' / 'nickname' / 'role' "
+                                 "/ 'strength' / 'symbol'"}), 400
+
+    if sort_asc in attributes:
+        sorted_characters = sorted(characters, key=lambda x: (x.get(sort_asc) is None, x.get(sort_asc)))
+        return jsonify(sorted_characters), 200
+
+    if sort_desc in attributes:
+        sorted_characters = sorted(characters, key=lambda x: (x.get(sort_desc) is None, x.get(sort_desc)), reverse=True)
+        return jsonify(sorted_characters), 200
+
+    # Pagination
     limit = 20
     skip = 0
 
@@ -36,7 +55,7 @@ def get_characters():
         return jsonify({"message": f"Skip is exceeding the length of the"
                                    f" characters database (Total characters: {len(characters)})"}), 400
 
-    if 'limit' not in request.args and 'skip' not in request.args:
+    if 'limit' not in request.args and 'skip' not in request.args and not sort_asc and not sort_desc:
         random_characters = random.sample(characters, min(20, len(characters)))
         return jsonify(random_characters), 200
 
