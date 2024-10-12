cafd5311f1ef25855e216a1c6af9e3f25f110ea1
mariacristinafadgyas <fadgyas.maria@yahoo.com>
2024-10-12 23:56:41 +0300

    deleted a module import that is causing some deployment issues

--- frontend/frontend_app.py
+++ frontend/frontend_app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, jsonify, url_for, request
-from backend.storage import read_data
-import os
 import requests
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
